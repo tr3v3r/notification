0ff8d2f099e756b68a8983986ba4c380946a8f02
Aliaksei Astafyeu <aliaksei_astafyeu@epbyminw8497.minsk.epam.com>
2019-12-11 17:02:11 +0300

    Intitial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const tipsData = [
+    'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium',
+    'eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat ',
+    'qui officia deserunt mollitia animi, id est laborum et dolorum fuga',
+    'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates',
+    'earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias',
+    'numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
+    'culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+]
+
+
